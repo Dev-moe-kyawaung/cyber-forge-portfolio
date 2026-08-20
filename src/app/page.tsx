308d96fa0a038c05b2e59520f6c5739111e31435
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-20 19:26:46 +0630

    Create HomePage component with sections

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { HeroSection } from "@/components/sections/HeroSection";
+import { ProjectsSection } from "@/components/sections/ProjectsSection";
+import { AboutSection } from "@/components/sections/AboutSection";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <ProjectsSection />
+      <AboutSection />
+    </>
+  );
+}
